af5e5c1134c9e126c6a02eb2f4ed84ee1fab600f
Fábio Sano <fabinhosano@hotmail.com>
2020-10-19 16:03:38 -0300

    d

--- src/server.js
+++ src/server.js
@@ -15,4 +15,19 @@ server.get('/lojas', pages.lojas)
 server.get('/localizacao', pages.localizacao)
 server.get('/create-loja', pages.createLoja)
 server.post('/save-loja', pages.saveLoja)
-server.listen(5500)
+server.listen(port,)
+
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`JSON Server is running in ${port}`);
+});
