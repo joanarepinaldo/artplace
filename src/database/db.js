b44f8fb0669389b85d761b2b06a9af6756848e74
Fábio Sano <fabinhosano@hotmail.com>
2021-07-09 12:37:41 -0300

    v1

--- src/database/db.js
+++ src/database/db.js
@@ -1,3 +1,14 @@
+async function connect(){
+    if(global.connection && global.connection.state !== 'disconnected')
+        return global.connection;
+ 
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection("mysql://root:luiztools@localhost:3306/crud");
+    console.log("Conectou no MySQL!");
+    global.connection = connection;
+    return connection;
+}
+
 const Database = require('sqlite-async');
 
 function execute(db){
