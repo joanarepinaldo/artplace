0e802fffff12da0c01241d857b5d6055dee81e2b
Fábio Sano <fabinhosano@hotmail.com>
2021-07-09 18:22:38 -0300

    vercel

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ conexao.connect((erro) =>{
         console.log(erro)
     }else{
         Tabelas.init(conexao);
-        server.listen(5500);
-        console.log("Conectado");
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
     }
 })
