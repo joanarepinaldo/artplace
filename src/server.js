c5ab1342db3e91bc65c1cb25cbae8d9663052f43
Fábio Sano <fabinhosano@hotmail.com>
2021-07-09 19:25:17 -0300

    d

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.static('public'));
-app.set ('views', path.join(__dirname, "views"))
+//app.set ('views', path.join(__dirname, "views"))
 //app.set('view engine', 'hbs')
 
 app.get('/', pages.index)
